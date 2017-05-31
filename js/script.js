8ddcc8b273f030db71c2dc769c93a2a81bd4575f
Slawomir Osiak <slawek-osiak@o2.pl>
2017-05-31 11:43:09 +0200

    delete triangle1 and triangle 2 and add var triangle1 and 2

--- js/script.js
+++ js/script.js
@@ -5,20 +5,9 @@ function getTriangleArea(a, h) {
 		return (a*h/2)
 	}
 }
-function getTriangle1Area(a, h) {
-	if (a < 0 || h<0) {
-		console.log('Nieprawidłowa wartość')
-	} else {
-		return (a*h/2)
-	}
-}
-function getTriangle2Area(a, h) {
-	if (a < 0 || h<0) {
-		console.log('Nieprawidłowa wartość')
-	} else {
-		return (a*h/2)
-	}
-}
 console.log(getTriangleArea(10, 6));
-console.log(getTriangle1Area(15, 6));
-console.log(getTriangle2Area(-2, 12));
+var triangle1Area = getTriangleArea(10, 6);
+var triangle2Area = getTriangleArea(20, 8);
+var triangle3Area = getTriangleArea(-2, 6);
+
+
